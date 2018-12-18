c5922b682e797094aa68dea752c07ae94b757846
Dylan Thiemann <dylanthiemann@gmail.com>
2018-12-18 13:19:02 -0800

    adding practice regex file

---
+++ regex.py
@@ -0,0 +1,32 @@
+# Regex - Automate the Boring Stuff
+
+import pyperclip, re
+
+# Phone Number
+# 1. area code
+# 2. separator
+# 3. first 3 digits
+# 4. last 4 digits
+# 5. extension
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?
+    (\s|-|\.)?
+    (\d{3})
+    (\d{4})
+    (\s*{ext|x|ext.)\s*(\d{2,5}))?
+)''', re.VERBOSE)
+
+# Email
+# 1. First segemnt (before @ - alphanumeric uncluding underscore and dash)
+# 2. @ symbole
+# 3. domain name
+# 4. dot-something
+emailRegex = re.compile(r'''(
+    ^[a-zA-Z0-9_-]+
+    (@)
+    [a-zA-Z0-9.-]+
+    (\.[a-zA-Z]{2,4})
+)''', re.VERBOSE)
+
+# Returns true if the password is strong, false if not
+def strongPassword(password):
