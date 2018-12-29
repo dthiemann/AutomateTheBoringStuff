d643244e163eac52422019138c8eb7e152be19e3
Dylan Thiemann <dylanthiemann@gmail.com>
2018-12-29 15:23:01 -0800

    some webparsing + comic downloader

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,4 +1,4 @@
-import webbrowser, sys, pyperclip
+import webbrowser, sys, pyperclip, requests, bs4, os
 
 # Web Scrapping - Automate The Boring Stuff - Chapter 11
 
@@ -7,6 +7,9 @@ def main():
     print("Main script")
     # openBrowserToUrl("https://www.google.com")
     # googleMapsAddress()
+    # getRequest("https://automatetheboringstuff.com/files/rj.txt")
+    # handleFailedRequest("http://inventwithpython.com/page_that_does_not_exist")
+    getXkcdComics()
 
 
 def openBrowserToUrl(url):
@@ -22,3 +25,69 @@ def googleMapsAddress():
 
     webbrowser.open("https://www.google.com/maps/place/" + address)
 
+
+# Handling requests
+def getRequest(url):
+    res = requests.get(url)
+    print(type(res), res)
+    print(len(res.text))
+    print(res.text[:100])
+
+
+def handleFailedRequest(url):
+    res = requests.get(url)
+    try:
+        res.raise_for_status()
+    except Exception as ex:
+        print("there was a problem: %s" % (ex))
+
+
+def getXkcdComics():
+    url = "http://xkcd.com"  # starting URL
+    os.makedirs("xkcd", exist_ok=True)  # store comics in ./xkcd`
+    i = 0
+    while not url.endswith("#") and i < 20:
+
+        # Download the page
+        i += 1
+
+        print("Downloading page %s..." % url)
+        res = requests.get(url)
+        res.raise_for_status()
+
+        soup = bs4.BeautifulSoup(res.text)
+
+        # TODO: Find the URL of the comic image
+
+        comicElem = soup.select("#comic img")
+        if comicElem == []:
+            print("Could not find comic image")
+        else:
+            try:
+                comicUrl = "http:" + comicElem[0].get("src")
+
+                # Download the image
+                print("Download image %s..." % (comicUrl))
+                res = requests.get(comicUrl)
+                res.raise_for_status()
+            except requests.exceptions.MissingSchema:
+                # skip this comic
+                prevLink = soup.select('a[rel="prev"]')[0]
+                url = "http://xkcd.com" + prevLink.get("href")
+                continue
+
+            # TODO: Save the image to ./xkcd
+
+            iamgeFile = open(os.path.join("xkcd", os.path.basename(comicUrl)), "wb")
+            for chunk in res.iter_content(100000):
+                iamgeFile.write(chunk)
+            iamgeFile.close()
+
+        # TODO: Get the Prev button's url.
+        prevLink = soup.select('a[rel="prev"]')[0]
+        url = "http://xkcd.com" + prevLink.get("href")
+
+    print("Done.")
+
+
+main()
