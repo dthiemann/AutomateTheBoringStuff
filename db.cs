5418c36006d3a1bcae8d7e504bdb1964e7e8dea7
Dylan Thiemann <dylanthiemann@gmail.com>
2018-12-30 12:42:29 -0800

    some base cs scripting code (nothing fancy) + local database information

---
+++ db.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class DatabaseProgram
+{
+    public void Test()
+    {
+        Console.WriteLine("Hello word from DB program");
+    }
+}
+
+var newClass = new DatabaseProgram();
+newClass.Test();
