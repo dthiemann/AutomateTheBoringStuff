ee7afd467849d37b6efddc5e7d877c17e7d3ed18
Dylan Thiemann <dylanthiemann@gmail.com>
2018-12-30 17:04:28 -0800

    finding the kth element from the end of a singly linked list

--- CrackingTheCodingInterview/linked_lists.py
+++ CrackingTheCodingInterview/linked_lists.py
@@ -31,6 +31,7 @@ class Node:
         self.nextNode = newNode
 
 
+# 1.1 Remove duplicates without a secondary data structure
 def removeDuplicates(startingNode):
     currentNode = startingNode
     while (currentNode != None and currentNode.getNext() != None):
@@ -52,14 +53,43 @@ startingLink.appendToTail(13)
 startingLink.appendToTail(14)
 startingLink.appendToTail(15)
 startingLink.appendToTail(16)
+startingLink.appendToTail(17)
+startingLink.appendToTail(18)
+startingLink.appendToTail(19)
+startingLink.appendToTail(20)
+startingLink.appendToTail(21)
+startingLink.appendToTail(22)
+startingLink.appendToTail(23)
+startingLink.appendToTail(24)
+startingLink.appendToTail(25)
+startingLink.appendToTail(26)
+startingLink.appendToTail(27)
 
-startingLink.appendToTail(12)
-startingLink.appendToTail(13)
-startingLink.appendToTail(14)
-startingLink.appendToTail(15)
-startingLink.appendToTail(16)
+# print("starting list")
+# startingLink.printLinkedList()
+# print("ending list")
+# removeDuplicates(startingLink)
 
-print("starting list")
-startingLink.printLinkedList()
-print("ending list")
-removeDuplicates(startingLink)
+# 1.2 Find the kth from the last elemnt of a list
+
+
+def findTheKthFromTheLast(startingNode, k):
+    # Get the running node
+    runner = startingNode
+    current = startingNode
+    i = 0
+    while (runner.getNext() != None and i < k):
+        runner = runner.getNext()
+        i += 1
+
+    if (i < k):
+        return None     # The list is not long enough
+
+    while (runner.getNext() != None):
+        current = current.getNext()
+        runner = runner.getNext()
+
+    return current
+
+
+print(findTheKthFromTheLast(startingLink, 5).getData())
